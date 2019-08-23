81bddd782d864aa73226b47fee162bde14ecee05
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-08-23 14:43:36 +0500

    functionality for user chats added

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/database";
 import Message from "../Models/Message.model";
 import { Observable } from "rxjs";
-import { UserService } from '../user.service';
+import { UserService } from "../user.service";
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.scss"]
 })
 export class ChatComponent implements OnInit {
-
   public users: number = 0;
   public showChatBox: boolean = true;
   public message: Message;
-  public messages: Message[] = [];
-  chats: Observable<any[]>;
+  private usersList = [];
+  public messages = [];
+  private userMessages: Observable<any[]>;
+  private usersConversation= [];
+  private currentUser;
+  private selectedUser;
 
   @ViewChild("msgRef") msgRef;
   constructor(
@@ -24,18 +27,36 @@ export class ChatComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.chats = this.db.list("chat").valueChanges();
-    console.log("chat user is : ", localStorage.getItem("chat-user"))
-    this.chats.subscribe(chatsObs => {
+    this.currentUser = JSON.parse(localStorage.getItem("chat-user"));
+    this.db.database.ref('users').on('value', snapShot => {
+      snapShot.forEach(user => {
+        let userObj = {
+          id: user.key,
+          name: user.val().name,
+          photo: user.val().photo
+        }
+        this.usersList.push(userObj)
+      })
+      this.usersList = this.usersList.filter(user => user.id != this.currentUser.id)
+    })
+    this.userMessages = this.db
+      .list(`users-chats/user||${this.currentUser.id}`)
+      .valueChanges();
+    this.userMessages.subscribe(chatsObs => {
       this.messages = [];
       chatsObs.forEach(chatRef => {
-        let newMessage: Message = {
-          user: chatRef.user,
-          date: chatRef.date,
-          content: chatRef.content
+        let newMessage = {
+          sender: chatRef.sender,
+          senderName: chatRef.senderName,
+          text: chatRef.text,
+          receiver: chatRef.receiver,
+          date: chatRef.date
         };
         this.messages.push(newMessage);
       });
+      if(this.selectedUser){
+        this.usersConversation = this.messages.filter(msg => msg.sender == this.selectedUser.id || msg.receiver == this.selectedUser.id)
+      }
     });
   }
 
@@ -46,16 +67,29 @@ export class ChatComponent implements OnInit {
   //   this.message = "";
   //   this.msgRef.nativeElement.value = "";
   // }
-
-  addMessage() {
-    let newMessage: Message = {
-      user: localStorage.getItem("chat-user"),
-      date: new Date().toISOString().split("T")[0],
-      content: this.msgRef.nativeElement.value
-    };
-    this.db.database.ref("chat").push(newMessage);
-    this.msgRef.nativeElement.value = ''
-
+  onUserClicked(user) {
+    this.selectedUser = user;
+    this.showChatBox = !this.showChatBox;
+    this.usersConversation = this.messages.filter(msg => msg.sender == this.selectedUser.id || msg.receiver == this.selectedUser.id)
+    console.log("selectedUser is : ", this.selectedUser)
   }
-
+  addMessage() {
+    // console.log("this.currentUser is : ", this.currentUser);
+    let newMessage = {
+      sender: this.currentUser.id,
+      senderName:  this.currentUser.name,
+      text: this.msgRef.nativeElement.value,
+      receiver: this.selectedUser.id,
+      date: new Date().toISOString().split("T")[0]
+    };
+    let senderDbRef = this.db.database.ref(
+      `users-chats/user||${this.currentUser.id}`
+    );
+    senderDbRef.push(newMessage);
+    let receiverDbRef = this.db.database.ref(
+      `users-chats/user||${this.selectedUser.id}`
+    );
+    receiverDbRef.push(newMessage);
+    this.msgRef.nativeElement.value = "";
+  }
 }
