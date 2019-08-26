4618a0617345ec03a69aac575bfc5eb8bad2ccf4
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-08-26 13:20:24 +0500

    added pie chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,8 @@ export class HomeComponent implements OnInit {
     // this.drawHeatMap();
     // this.drawSankeyDiagram();
     // this.drawSunburstDiagram();
-    this.drawScatterBubble();
+    // this.drawScatterBubble();
+    this.drawPieChart();
   }
 
   drawHeatMap() {
@@ -1153,15 +1154,84 @@ export class HomeComponent implements OnInit {
           .attr("cy", function(d) {
             return y(d.Petal_Length);
           })
-          .attr("r", function(d){
-            console.log("d is : ", d)
-            return parseFloat(d.Sepal_Length)
+          .attr("r", function(d) {
+            console.log("d is : ", d);
+            return parseFloat(d.Sepal_Length);
           })
           .style("fill", function(d) {
             return color(d.Species);
           });
       }
     );
+  }
+
+  drawPieChart() {
+    var width = 450,
+    height = 450,
+    margin = 40;
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin;
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3
+      .select("#pie")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Create dummy data
+    var data = { a: 5, b: 20, c: 30, d: 8, e: 15 };
+
+    // set the color scale
+    var color = d3
+      .scaleOrdinal()
+      .domain(data)
+      .range(["#ff0000", "#00ff00", "#0000ff", "#F4D03F", "#f5f5f5"]);
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie().value(function(d) {
+      return d.value;
+    });
+    var data_ready = pie(d3.entries(data));
+    // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+    // shape helper to build arcs:
+    var arcGenerator = d3
+      .arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+      .selectAll("mySlices")
+      .data(data_ready)
+      .enter()
+      .append("path")
+      .attr("d", arcGenerator)
+      .attr("fill", function(d) {
+        return color(d.data.key);
+      })
+      .attr("stroke", "black")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7);
+
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    svg
+      .selectAll("mySlices")
+      .data(data_ready)
+      .enter()
+      .append("text")
+      .text(function(d) {
+        return d.data.value;
+      })
+      .attr("transform", function(d) {
+        return "translate(" + arcGenerator.centroid(d) + ")";
+      })
+      .style("text-anchor", "middle")
+      .style("font-size", 17);
   }
 
   signInWithGoogle() {
